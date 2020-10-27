4a0fe5564d14c324bd2352cc583ca86da48cc096
Illia-tsar <micloytv@gmail.com>
2020-10-27 16:10:02 +0000

    added Constructor and Destructor

--- Filters.cpp
+++ Filters.cpp
@@ -2,4 +2,11 @@
 // Created by linups on 10/27/20.
 //
 
+#include <iostream>
 #include "Filters.h"
+Filters::Filters() {
+    std::cout<<"Filters created!"<<std::endl;
+}
+Filters::~Filters() {
+    std::cout << "Filters deleted!" << std::endl;
+}
