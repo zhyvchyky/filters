8fe7480ed1932dda86aa84c6998eeddead0a9d82
Volodymyr Tesliuk <vovatesluk6a@gmail.com>
2020-11-24 18:04:01 +0200

    added basic implementation of ImageOutputProcessor; << operator for Pixel overloaded

---
+++ processors/ImageOutputProcessor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by noxin on 11/24/20.
+//
+
+#include "ImageFileWriteProcessor.h"
---
+++ processors/ImageOutputProcessor.h
@@ -0,0 +1,15 @@
+//
+// Created by noxin on 11/24/20.
+//
+
+#ifndef FILTERS_IMAGEFILEWRITEPROCESSOR_H
+#define FILTERS_IMAGEFILEWRITEPROCESSOR_H
+
+#include "IProcessor.h"
+
+class ImageOutputProcessor : IProcessor{
+    void writeImageToFile
+};
+
+
+#endif //FILTERS_IMAGEFILEWRITEPROCESSOR_H
